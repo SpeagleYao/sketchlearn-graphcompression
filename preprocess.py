43341ae8ca3d40838dbc647dac2bfb416f2ebe3a
milliondegree <zhangjiachi1007@gmail.com>
2021-06-11 11:11:26 +0800

    sample new

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,23 @@
 import pandas as pd
 import numpy as np
 
-def sample(data, sampled, width, n, size, refer):
+
+# def parse():
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument('-n', type=int, default=20)
+#   parser.add_argument('-d', type=int, default=2)
+#   parser.add_argument('-w', type=int, default=5)
+#   return parser.parse_args()
+
+
+def sample(data, sampled, width, size, refer):
+  '''
+  data: original pd frame data
+  sampled: sampled pd frame data
+  width: max width to search during dfs
+  size: total number of tuples to sample
+  refer: start node
+  '''
   # tmp = pd.DataFrame(columns=data.columns)
   if sampled.shape[0]>=size:
     return sampled
@@ -14,15 +30,15 @@ def sample(data, sampled, width, n, size, refer):
   i = 0
   count = 0
   while count<width and i<row:
-    if indice[i] not in sampled.index and whole.loc[indice[i],'node1']+whole.loc[indice[i],'node2']<40:
+    # if indice[i] not in sampled.index and whole.loc[indice[i],'node1']+whole.loc[indice[i],'node2']<40:
+    if indice[i] not in sampled.index:
       sampled = sampled.append(whole.loc[indice[i]])
-      n += 1
       count += 1
-      if n==size:
+      if sampled.shape[0]==size:
         return sampled
     i += 1
   r = np.random.choice(sampled[['node1', 'node2']].values.reshape(-1))
-  return sample(data, sampled, width, n, size, r)
+  return sample(data, sampled, width, size, r)
 
 
 def preprocess(sampled):
@@ -37,12 +53,14 @@ def preprocess(sampled):
 
 if __name__=='__main__':
   rawfile = './data/trust-new/bitcoinotc-prob.csv'
-  savefile = './data/trust-new/sample_20_new.csv'
+  savefile = './data/sample_20_new.csv'
   rawdata = pd.read_csv(rawfile, sep=',')
+  rawdata = rawdata[rawdata['node1']<30][rawdata['node2']<30]
+  # print(rawdata.shape[0])
   # print(rawdata)
   sampled = pd.DataFrame(columns=rawdata.columns)
-  sampled = sample(rawdata, sampled, 3, 0, 20, 1)
-  sampled = preprocess(sampled)
+  sampled = sample(rawdata, sampled, 5, 10, 1)
+  # sampled = preprocess(sampled)
   print(sampled)
   sampled.to_csv(savefile, encoding='gbk', index=False)
 
