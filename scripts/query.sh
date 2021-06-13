52a33ddb6363e9fab8c9b45f44df4258a488be54
yiran <speagle@aries.cs-georgetown.net>
2021-06-13 00:54:35 -0400

    .

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,7 +1,11 @@
-LOG_FILE="./data/sample10_new_10rounds/sample10_new_10rounds_mutualtrust1-2.log"
-SKETCH_FILE="./data/sample10_new_10rounds/sample_10_new_10rounds.prov"
-PROB_FILE="./data/sample10_new_10rounds/sample_10_new.csv"
-PROV_FILE="./data/sample10_new_10rounds/mutualtrust1-2.txt"
+# LOG_FILE="./data/sample10_new_10rounds/sample10_new_10rounds_mutualtrust1-2.log"
+# SKETCH_FILE="./data/sample10_new_10rounds/sample_10_new_10rounds.prov"
+# PROB_FILE="./data/sample10_new_10rounds/sample_10_new.csv"
+# PROV_FILE="./data/sample10_new_10rounds/mutualtrust1-2.txt"
+LOG_FILE="./data/prob_test5/sample5_10rounds_mutualtrust1-2.log"
+SKETCH_FILE="./data/prob_test5/sample5_10rounds.prov"
+PROB_FILE="./data/prob_test5/sample5.csv" # ?
+PROV_FILE="./data/prob_test5/mutualtrust1-2.txt" # ?
 rm $LOG_FILE
 for p in 0.6 0.7 0.8 0.9 1.0
 do 
